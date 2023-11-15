2b7bb454bb51abe70d729eb693b7812f9b90928a
Kundu, Mohit <mkundu@iu.edu>
2023-11-15 12:24:16 -0500

    Create events.js

---
+++ src/models/events.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+
+const eventSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  image: {
+    type: Buffer,
+    required: false,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  organizer: {
+    type: String,
+    required: true,
+  },
+  // organizer: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'user',
+  // },
+  attendees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+  price: {
+    type: Number,
+    required: true,
+  }
+}, {
+  timestamps: true, // Automatically add 'createdAt' and 'updatedAt' fields
+});
+
+// Create the Event model
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
