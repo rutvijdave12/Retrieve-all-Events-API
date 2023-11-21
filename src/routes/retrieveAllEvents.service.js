8e9cf7eb211602621072101232848be93b8dc682
mkundu <mkundu@iu.edu>
2023-11-20 23:32:18 -0500

    Updated port number

--- src/routes/retrieveAllEvents.service.js
+++ src/routes/retrieveAllEvents.service.js
@@ -1,10 +1,32 @@
 const Event = require('../models/events');
+const errors = require('../../errors/errors');
 
-// Retrieve all events
-exports.getAllEvents = async () => {
+exports.getAllEvents = async (req, res) => {
   try {
-    return await Event.find();
+    const events = await Event.find();
+
+    return res.status(200).json({
+      statusCode: 0,
+      timestamp: Date.now(),
+      requestId: req.body.requestId,
+      data: events,
+      info: {
+        code: errors['000'].code,
+        message: errors['000'].message,
+        displayText: errors['000'].displayText,
+      },
+    });
   } catch (error) {
-    throw error;
+    return res.status(500).json({
+      statusCode: 1,
+      timestamp: Date.now(),
+      requestId: req.body.requestId,
+      info: {
+        code: errors['006'].code,
+        message: error.message || errors['006'].message,
+        displayText: errors['006'].displayText,
+      },
+      error: error,
+    });
   }
 };
